4b8f45b557d44f2b1d09a7227466337130ddea49
Bitteswargit <bitteswarpradhan@gmail.com>
2025-09-08 12:01:10 +0530

    new changes after forammting

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -6,13 +6,13 @@ module "eks" {
   cluster_name    = local.cluster_name
   cluster_version = "1.29"
 
-  vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
+  vpc_id                         = module.vpc.vpc_id
+  subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
   # KMS key configuration - automatically created with unique alias
   create_kms_key = true
-  kms_key_alias  = "alias/eks/${local.cluster_name}-key"  # unique alias per cluster
+  kms_key_alias  = "alias/eks/${local.cluster_name}-key"
 
   # Managed Node Group defaults
   eks_managed_node_group_defaults = {
